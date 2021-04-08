b7d1d132acff9e4cdf3d45d11fb482ec8387286a
Thiago Tassinari <thiagotassinari12@gmail.com>
2021-04-08 09:58:33 -0300

    Ajustes no menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Alimentos', url: '/folder/Alimentos', icon: 'fast-food' },
+    { title: 'Receitas', url: '/folder/Receitas', icon: 'book' },
+    { title: 'Estoque', url: '/folder/Estoque', icon: 'file-tray-stacked' },
+    { title: 'Pedidos', url: '/folder/Pedidos', icon: 'receipt' },
+    { title: 'Perdas e Desperdicios', url: '/folder/Perdas_Desperdicios', icon: 'trash' }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
